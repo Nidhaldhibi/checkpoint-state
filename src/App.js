86c733ee551f56dbb328fcab46ef43c079d46ded
dhibi nidhal <dhibinidhal@gmail.com>
2023-02-06 21:46:15 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Counter from "./components/TimeInterval";
+import photo from "./components/nidhal.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    fullName: "DHIBI Nidhal",
+    bio: "java script full stack developer,part of team hackerspace Gafsa",
+    imgSrc: { photo },
+    show: false,
+  };
+  toggleVisibility = () => {
+    this.setState({ show: !this.state.show });
+  };
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.toggleVisibility}>show</button>
+        {this.state.show ? (
+          <div>
+            <h1>{this.state.fullName}</h1>
+            <p>{this.state.bio}</p>
+            <img src={photo} alt="nidhal.jpg" />
+          </div>
+        ) : (
+          <p> button show </p>
+        
+        )}
+        <Counter />
+      </div>
+    );
+  }
 }
 
 export default App;
